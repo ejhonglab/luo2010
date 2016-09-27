11b90389224104ece2752429bd4798f4ff722945
Tom O'Connell <toconnel@caltech.edu>
2016-09-27 01:30:39 -0700

    Added the apparent explanation for why some firing rates are negative after sum.

--- luo2010.py
+++ luo2010.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+# prevents white lines from being overlayed over data
 sns.set_style('dark')
 
 """
@@ -15,17 +16,49 @@ A little bit about the Hallem and Carlson 2006 dataset:
     -flies are <4 weeks old (but how young on average?)
     -24 ml/s carrier with 5.9 ml/s odor stream
     -female flies (??)
+
+    TODO: my interpretation of why spontaneous - diff is sometimes negative is that
+    the diff is calculated using (in at least the 2004 paper, was unclear in 2006)
+    the second (maybe 0.5 sec here) before and the second after (with 500 ms pulse
+    in their 2004 paper)
+    -if this is the case, there could just be an unusually busy period preceding the
+     stimulus, exagerating the inhibition
+"""
+
+"""
+Fig 1a: raw Hallem and Carlson 2006
 """
 
 # skip glomerulus labels, which are not assigned to each response
 # keep the receptor labels, which are assigned to each response
 hc06 = pd.read_csv('~/Dropbox/Hallem_Carlson_2006.csv', skiprows=1)
-hc06 = hc06.set_index(['receptor'])
+
+# first column of first row manually set to this in the CSV data file
+# might want to use more pandonic way of doing this
+hc06 = hc06.set_index(['odor'])
+hc06.columns.name = 'receptor'
 
 # way to add the labels with less handles?
 fig = plt.figure()
 # the 111 means "1x1 grid, first subplot"
 ax = fig.add_subplot(111)
+
+# exclude the last row, because those are spontaneous firing rates
+delta_orn = hc06.as_matrix()[:-1,:]
+
+# get the spontaneous rates so we can add them back
+spont_orn = hc06.loc[hc06.index == 'spontaneous firing rate'].as_matrix().flatten()
+
+# recover actual firing rates (in the 500ms binning windows)
+orn = np.empty_like(delta_orn) * np.nan
+
+# TODO numpy way to do this broadcasting?
+
+# for each odor add the spontaneous rates to the difference observed for that odor
+# adds the vector of spontaneous rates across all receptors to the odor specific
+# vector of reponses across receptors
+for i in range(delta_orn.shape[0]):
+    orn[i,:] = delta_orn[i,:] + spont_orn
 
 cax = ax.matshow(hc06.as_matrix(), cmap=plt.cm.viridis, aspect=0.3) #aspect='auto')
 
@@ -62,4 +95,6 @@ ax.set_yticklabels(hc06.index.values, fontsize=6)
 
 plt.show()
 
-
+"""
+Fig 1b: 
+"""
